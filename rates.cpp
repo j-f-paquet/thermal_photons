98efa71e6f798e90802bc1ffa5817a89dc12098e
J-F Paquet <jeanfrancois.paquet@duke.edu>
2019-10-15 07:45:06 -0700

    Reorganized and cleaned up the rate file

--- rates.cpp
+++ rates.cpp
@@ -1,39 +1,8 @@
 #include "photon.h"
+#include "rates.h"
 
 /***** Rates *****/
-
 void init_rates(struct photonRate * currRate, enum rate_type id) {
-
-	//
-	double rate_thermal_ideal(double,double,double);
-	double rate_qgp_ideal_born_AMYfit(double, double, double);
-	double rate_qgp_ideal_born_KLS(double, double, double);
-	double rate_qgp_ideal_born_JF_sqrtg(double kOverT, double T, double kkPiOver_e_P_k2);
-	double rate_qgp_viscous_only_born_JF_sqrtg(double kOverT, double T, double kkPiOver_e_P_k2);
-	double rate_hg_ideal_Turbide_fit(double kOverT, double T, double kkPiOver_e_P_k2);
-	double rate_qgp_ideal_LO_AMYfit(double, double, double);
-	double rate_qgp_ideal_born_AMYfit_with_cuts(double kOverT, double T, double kkPiOver_e_P_k2);
-	double rate_qgp_viscous_only_born_g2_sqrtg(double kOverT, double T, double kkPiOver_e_P_k2); 
-	double rate_hg_ideal_Turbide_fit_chem_pot_Boltz_Tch150(double kOverT, double T, double kkPiOver_e_P_k2);
-	double rate_hg_ideal_Turbide_fit_chem_pot_Boltz_Tch160(double kOverT, double T, double kkPiOver_e_P_k2);
-	double rate_hg_ideal_Turbide_fit_chem_pot_Boltz_Tch165(double kOverT, double T, double kkPiOver_e_P_k2);
-	double T2_normalisation(double kOverT, double T, double kk);
-	double minus_sign_normalisation(double kOverT, double T, double kk);
-	double rate_qgp_viscous_only_born_g2_sqrtg_fit2(double kOverT, double T, double kkPiOver_e_P_k2); 
-	double rate_qgp_viscous_only_born_g2_sqrtg_new_deltaf(double kOverT, double T, double kkPiOver_e_P_k2); 
-	double rate_qgp_viscous_only_born_g2_sqrtg_new_deltaf_cuts(double kOverT, double T, double kkPiOver_e_P_k2); 
-	double rate_qgp_viscous_only_born_g2_sqrtg_new_deltaf_fit2(double kOverT, double T, double kkPiOver_e_P_k2); 
-	double rate_qgp_ideal_Dusling(double kOverT, double T, double kkPiOver_e_P_k2);
-	double rate_qgp_shear_viscous_Dusling(double kOverT, double T, double kkPiOver_e_P_k2);
-	double rate_qgp_bulk_viscous_Dusling(double kOverT, double T, double kkPiOver_e_P_k2);
-	double rate_hg_in_medium_rho_ideal_Rapp_fit(double kOverT, double T, double kkPiOver_e_P_k2); 
-	double rate_hg_ideal_Turbide_noPiPi_fit(double kOverT, double T, double kkPiOver_e_P_k2);
-	double rate_hg_pion_brem_ideal_Rapp_fit(double kOverT, double T, double kkPiOver_e_P_k2);
-	double rate_hg_ideal_Zahed_Dusling_2pi_fit(double kOverT, double T, double kkPiOver_e_P_k2);
-	double rate_hg_piRhoOmega_ideal_Rapp_fit(double kOverT, double T, double kkPiOver_e_P_k2);
-
-	void tabulate_fit(struct photonRate * currRate); 
-	void load_rate_from_file(struct photonRate * currRate);
 
 	switch(id) {
 
@@ -287,6 +256,18 @@ void init_rates(struct photonRate * currRate, enum rate_type id) {
 			currRate->temp_discretization_type=linear;
 
 			tabulate_fit(currRate);
+			break;
+
+		//rate_qgp_ideal_LO_AMYfit
+		case qgp_ideal_LO_AMYfit_orig:
+			
+			currRate->name="qgp_ideal_LO_AMYfit_orig";
+			
+			currRate->is_qgp=true;
+
+			currRate->use_table_instead_of_fit=false;
+			currRate->tabulate_fit_for_speed=false;
+			currRate->rate_fit_function=rate_qgp_ideal_LO_AMYfit;
 			break;
 
 		//rate_qgp_ideal_LO_AMYfit
@@ -976,59 +957,6 @@ double eval_photon_rate(const struct photonRate * currRate, double kOverT, doubl
 
 	return res;
 }
-
-/*
-Rates:
-1: double rate_qgp_ideal_born_AMYfit(double kOverT, double T, double kkPiOver_e_P_k2);
-2: double rate_qgp_ideal_born_KLS(double kOverT, double T, double kkPiOver_e_P_k2);
-3: double rate_qgp_ideal_born_JF_sqrtg(double kOverT, double T, double kkPiOver_e_P_k2);
-4: double rate_qgp_viscous_only_born_JF_sqrtg(double kOverT, double T, double kkPiOver_e_P_k2);
-5: rate_hg_ideal_Turbide_fit
-6: rate_qgp_ideal_LO_AMYfit
-*/
-
-
-//void get_photon_rate(int selector, double (**local_rate)(double, double, double)) {
-//
-//	double rate_qgp_ideal_born_AMYfit(double, double, double);
-//	double rate_qgp_ideal_born_KLS(double, double, double);
-//	double rate_qgp_ideal_born_JF_sqrtg(double kOverT, double T, double kkPiOver_e_P_k2);
-//	double rate_qgp_viscous_only_born_JF_sqrtg(double kOverT, double T, double kkPiOver_e_P_k2);
-//	double rate_hg_ideal_Turbide_fit(double kOverT, double T, double kkPiOver_e_P_k2);
-//	double rate_qgp_ideal_LO_AMYfit(double, double, double);
-//
-//	//double (*local_rate)(double, double, double) = CONST_rateList[iRate].c_str();
-//	switch(selector) {
-//		case 1:
-//			//double (*local_rate)(double, double, double) = rate_qgp_ideal_born_AMYfit;
-//			*local_rate = rate_qgp_ideal_born_AMYfit;
-////			*rate_name= "rate_qgp_ideal_born_AMYfit";
-//			break;
-//		case 2: 
-//			*local_rate = rate_qgp_ideal_born_KLS;
-////			*rate_name="rate_qgp_ideal_born_KLS";
-//			break;
-//		case 3: 
-//			*local_rate = rate_qgp_ideal_born_JF_sqrtg;
-////			*rate_name="rate_qgp_ideal_born_JF_sqrtg";
-//			break;
-//		case 4:
-//			*local_rate = rate_qgp_viscous_only_born_JF_sqrtg;
-////			*rate_name="rate_qgp_viscous_only_born_JF_sqrtg";
-//			break;
-//		case 5:
-//			*local_rate = rate_hg_ideal_Turbide_fit;
-////			*rate_name="rate_hg_ideal_Turbide_fit";
-//			break;
-//		case 6:
-//			*local_rate = rate_qgp_ideal_LO_AMYfit;
-////			*rate_name="rate_qgp_ideal_LO_AMYfit";
-//			break;
-//	}
-//
-//
-//
-//}
 
 double interp_x_from_index(enum interp_type interp_type, double xmin, double xmax, int i, int size) {
 
---
+++ rates.h
@@ -0,0 +1,33 @@
+// Rate functions
+double rate_thermal_ideal(double,double,double);
+double rate_qgp_ideal_born_AMYfit(double, double, double);
+double rate_qgp_ideal_born_KLS(double, double, double);
+double rate_qgp_ideal_born_JF_sqrtg(double kOverT, double T, double kkPiOver_e_P_k2);
+double rate_qgp_viscous_only_born_JF_sqrtg(double kOverT, double T, double kkPiOver_e_P_k2);
+double rate_hg_ideal_Turbide_fit(double kOverT, double T, double kkPiOver_e_P_k2);
+double rate_qgp_ideal_LO_AMYfit(double, double, double);
+double rate_qgp_ideal_born_AMYfit_with_cuts(double kOverT, double T, double kkPiOver_e_P_k2);
+double rate_qgp_viscous_only_born_g2_sqrtg(double kOverT, double T, double kkPiOver_e_P_k2); 
+double rate_hg_ideal_Turbide_fit_chem_pot_Boltz_Tch150(double kOverT, double T, double kkPiOver_e_P_k2);
+double rate_hg_ideal_Turbide_fit_chem_pot_Boltz_Tch160(double kOverT, double T, double kkPiOver_e_P_k2);
+double rate_hg_ideal_Turbide_fit_chem_pot_Boltz_Tch165(double kOverT, double T, double kkPiOver_e_P_k2);
+double rate_qgp_viscous_only_born_g2_sqrtg_fit2(double kOverT, double T, double kkPiOver_e_P_k2); 
+double rate_qgp_viscous_only_born_g2_sqrtg_new_deltaf(double kOverT, double T, double kkPiOver_e_P_k2); 
+double rate_qgp_viscous_only_born_g2_sqrtg_new_deltaf_cuts(double kOverT, double T, double kkPiOver_e_P_k2); 
+double rate_qgp_viscous_only_born_g2_sqrtg_new_deltaf_fit2(double kOverT, double T, double kkPiOver_e_P_k2); 
+double rate_qgp_ideal_Dusling(double kOverT, double T, double kkPiOver_e_P_k2);
+double rate_qgp_shear_viscous_Dusling(double kOverT, double T, double kkPiOver_e_P_k2);
+double rate_qgp_bulk_viscous_Dusling(double kOverT, double T, double kkPiOver_e_P_k2);
+double rate_hg_in_medium_rho_ideal_Rapp_fit(double kOverT, double T, double kkPiOver_e_P_k2); 
+double rate_hg_ideal_Turbide_noPiPi_fit(double kOverT, double T, double kkPiOver_e_P_k2);
+double rate_hg_pion_brem_ideal_Rapp_fit(double kOverT, double T, double kkPiOver_e_P_k2);
+double rate_hg_ideal_Zahed_Dusling_2pi_fit(double kOverT, double T, double kkPiOver_e_P_k2);
+double rate_hg_piRhoOmega_ideal_Rapp_fit(double kOverT, double T, double kkPiOver_e_P_k2);
+
+// Rate-related functions
+double T2_normalisation(double kOverT, double T, double kk);
+double minus_sign_normalisation(double kOverT, double T, double kk);
+
+// Helper functions
+void tabulate_fit(struct photonRate * currRate); 
+void load_rate_from_file(struct photonRate * currRate);
